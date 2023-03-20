bef69db5111c2fdc43e322f48a1d23f7413edda6
jaredjost <jaredbjost@gmail.com>
2023-03-19 21:41:51 -0400

    Updated packages

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ require('dotenv').config();
 
 app.use(bodyParser());
 
-const CLIENT_ID = 'a892135e04e60ac990969c510b4a88fadbe525abc269593ef25b0f0ebff06143';
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.CALLBACK_URL;
-
 app.use(express.static(__dirname));
 
 app.get('/healthz', (req, res) => {
@@ -21,9 +17,9 @@ app.post('/auth/exchange', (req, res) => {
   axios.post('https://api.planningcenteronline.com/oauth/token', {
     "grant_type": "authorization_code",
     "code": CODE,
-    "client_id": CLIENT_ID,
-    "client_secret": CLIENT_SECRET,
-    "redirect_uri": REDIRECT_URI
+    "client_id": process.env.CLIENT_ID,
+    "client_secret": process.env.CLIENT_SECRET,
+    "redirect_uri": process.env.REDIRECT_URI
   }).then(result => {
     const accessToken = result.data.access_token;
     const expiresIn = result.data.expires_in;
@@ -35,5 +31,10 @@ app.post('/auth/exchange', (req, res) => {
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log('App running on localhost:8080');
+  var port = process.env.PORT;
+  if (process.env.PORT == null)
+  {
+    port = "8080";
+  }
+  console.log('Server app listening on port ' + port);
 });
